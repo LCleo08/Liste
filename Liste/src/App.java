0eac04e8d15faa4a93bc8004625e2f23aac78471
leonardo cazzanti <leonardocazzanti@gmail.com>
2025-12-16 07:45:36 +0000

    Compiti

--- Liste/src/App.java
+++ Liste/src/App.java
@@ -1,10 +1,15 @@
 public class App {
-        public static void main(String[] args) {
-        Lista l=new Lista();
+    public static void main(String[] args){
+        Lista l = new Lista();
         l.add(new Nodo("Mario"));
         l.add(new Nodo("Giulio"));
-        l.add(new Nodo("Marco"));
         l.addHead(new Nodo("Gianni"));
         System.out.println(l);
+
+        boolean esiste = l.exists("Mario");
+        System.out.println("Esiste 'Mario'? " + esiste);
+
+        l.remove("Giulio");
+        System.out.println("Dopo remove 'Giulio': " + l);
     }
 }
--- Liste/src/Lista.java
+++ Liste/src/Lista.java
@@ -14,11 +14,38 @@ public class Lista {
     root = n;
 }
 
-    public void remove(String s){
+public void remove(String s) {
+        if (root == null) return;
 
+        if (!exists(s)) return;
+
+        if (s == null ? root.getValue() == null : s.equals(root.getValue())) {
+            root = root.getNext();
+            return;
+        }
+
+        Nodo prev = root;
+        Nodo curr = root.getNext();
+        while (curr != null) {
+            if (s == null ? curr.getValue() == null : s.equals(curr.getValue())) {
+                prev.setNext(curr.getNext());
+                return;
+            }
+            prev = curr;
+            curr = curr.getNext();
+        }
     }
-    public boolean exsist(String s){
-        return true;
+    public boolean exists(String s) {
+        Nodo temp = root;
+        while (temp != null) {
+            if (s == null) {
+                if (temp.getValue() == null) return true;
+            } else {
+                if (s.equals(temp.getValue())) return true;
+            }
+            temp = temp.getNext();
+        }
+        return false;
     }
 
     public Lista(){
@@ -50,6 +77,9 @@ public class Lista {
         return s + "\n";
                 
     }
+
+
+
 
 
     
